eca0c1a636bd338fb92fc5a231f96a80913c58ff
Mateus <goncalvesouza1996@gmail.com>
2024-07-24 13:44:50 -0300

    feat: adicionando código de polindrome

--- javascript_completo_udemy-master/javascript_completo_udemy-master/7_EXERCICIOS/Desafio03Seção7-FunçãoPolindrome/scripts.js
+++ javascript_completo_udemy-master/javascript_completo_udemy-master/7_EXERCICIOS/Desafio03Seção7-FunçãoPolindrome/scripts.js
@@ -1,7 +1,16 @@
-function soma(num1, num2, num3){
-  return num1 + num2 + num3;
+function isPalindrome(palavra) {
+  let palavraIndaVolta = palavra.replace(/[^A-Za-z0-9]/g, '').toLowerCase();
+
+  let comprimento = palavraIndaVolta.length;
+
+  for (let i = 0; i < comprimento / 2; i++) {
+    if (palavraIndaVolta[i] !== palavraIndaVolta[comprimento - 1 -i]){
+      return false;
+    }
 }
-
-const resultado = soma(5, 10, 15);
-
-console.log(resultado);
+   return true;
+}
+console.log(isPalindrome('radar'));
+console.log(isPalindrome('tenet'));
+console.log(isPalindrome(' A man, a plan, a canal, Panama '));
+console.log(isPalindrome('12321'));
