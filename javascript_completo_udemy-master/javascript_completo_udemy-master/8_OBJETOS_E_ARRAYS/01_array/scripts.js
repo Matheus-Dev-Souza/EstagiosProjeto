68914380b58fbe21d8992640d851f30495638ac6
Mateus <goncalvesouza1996@gmail.com>
2024-07-24 13:30:18 -0300

    feat: adicionando código de some de funções

--- javascript_completo_udemy-master/javascript_completo_udemy-master/8_OBJETOS_E_ARRAYS/01_array/scripts.js
+++ javascript_completo_udemy-master/javascript_completo_udemy-master/8_OBJETOS_E_ARRAYS/01_array/scripts.js
@@ -1,4 +1,4 @@
-let arr = [1, 4, 5, 6, 7];
+/* let arr = [1, 4, 5, 6, 7];
 let nomes = ['Matheus', 'João', 'Aléxia'];
 let bool = [true, false, true];
 
@@ -7,4 +7,21 @@ let misturado = [1, 'Matheus', true];
 console.log(arr);
 console.log(nomes);
 console.log(bool);
-console.log(misturado);
+console.log(misturado); */
+
+document.getElementById('showArraysBtn').addEventListener('click', function() {
+    let arr = [1, 4, 5, 6, 7];
+    let nomes = ['Matheus', 'João', 'Aléxia'];
+    let bool = [true, false, true];
+    let misturado = [1, 'Matheus', true];
+    
+  
+    document.getElementById('arrOutput').textContent = JSON.stringify(arr);
+    document.getElementById('nomesOutput').textContent = JSON.stringify(nomes);
+    document.getElementById('boolOutput').textContent = JSON.stringify(bool);
+    document.getElementById('misturadoOutput').textContent = JSON.stringify(misturado);
+  
+    document.getElementById('arraysContainer').classList.remove('hidden');
+  });
+  
+  console.log('arr');
